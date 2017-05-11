bb2443d8fc2c2e2342d1877051ac034cdf11af7f
Johnny <jr347@njit.edu>
2017-05-10 22:09:49 -0400

    First commit

---
+++ installQA.sh
@@ -0,0 +1,8 @@
+packageNum=`./getNum.php FEpackage-v | xargs`
+sudo cp /home/johnny/Desktop/feqav-$packageNum.tar.gz /var/www/html/
+rm -r feqav-$packageNum.tar.gz
+
+cd /var/www/html
+
+sudo tar -zxvf feqav-$packageNum.tar.gz
+
